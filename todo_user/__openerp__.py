34e7034696f4c97f5464ff6dc27f6d34d6d0c19c
Luigi Sison <lsison@moxylus.com>
2016-03-11 15:50:57 -0800

    Update __openerp__.py

--- todo_user/__openerp__.py
+++ todo_user/__openerp__.py
@@ -3,6 +3,6 @@
 { 'name': 'Multiuser To-Do',
   'description': 'Extend the To-Do app to multiuser.',
   'author': 'Daniel Reis',
-  'depends': ['todo_app']
+  'depends': ['todo_app'],
   'application': True,
 }
