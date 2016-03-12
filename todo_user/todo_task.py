3aca88cfa559855dcfb35bb2c4ab6afc6c1b7159
Luigi Sison <lsison@moxylus.com>
2016-03-11 17:23:24 -0800

    Update todo_task.py

--- todo_user/todo_task.py
+++ todo_user/todo_task.py
@@ -4,3 +4,4 @@ class TodoTask(models.Model):
   _inherit = 'todo.task'
   user_id = fields.Many2one('res.users', 'Responsible')
   date_deadline = fields.Date('Deadline')
+  name = fields.Char(help="What needs to be done?")
